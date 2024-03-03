c292477b8fbfa87ae394b7d4e79c2df4314dc94a
saba <89579913+sabaFitwi@users.noreply.github.com>
2024-03-03 18:22:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Portfolio - My personal project
+
+- **Start Date:** 30.05.2022
+
+## Brief
+
+This is a personal project to create a working professional portfolio website to attract future job opportunities
+
+## Programming languages and frameworks:
+
+- HTML5
+- JavaScript (Vanilla)
+- CSS
+- Adobe-xd
+- Figma
+
+## To do list:
+- Adding more projects
+- Adding  my CV
+-
---
+++ js/script.js
@@ -0,0 +1,130 @@
+// Hiding preloader
+window.addEventListener("load", () => {
+  document.querySelector(".preloader").classList.add("hidePreloader");
+  document.body.style.overflow = "visible";
+});
+const elements = document.querySelectorAll(".scroll-down");
+window.addEventListener("scroll", slideUp);
+function slideUp() {
+  for (var i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    const distInView =
+      element.getBoundingClientRect().top - window.innerHeight + 30;
+    if (distInView < 0) {
+      element.classList.add("inView");
+    } else {
+      element.classList.remove("inView");
+    }
+  }
+}
+slideUp();
+
+// navbar toggle
+const navButton = document.querySelector(".navbar-toggler");
+const navMenu = document.querySelector(".navbar-collapse");
+navButton.addEventListener("click", () => {
+  navMenu.classList.toggle("toggleNav");
+});
+document.querySelectorAll(".nav-link").forEach((link) =>
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("toggleNav");
+  })
+);
+
+// stopping transition during resizing
+let transitionStopper;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-transition-stopper");
+  clearTimeout(transitionStopper);
+  transitionStopper = setTimeout(() => {
+    document.body.classList.remove("resize-transition-stopper");
+  }, 400);
+});
+
+// showing navbar after scrolling
+window.addEventListener("scroll", () => {
+  let position =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0;
+  const navbar = document.querySelector(".navbar");
+  if (position >= 0) {
+    navbar.style.display = "block";
+  } else {
+    navbar.style.display = "none";
+  }
+});
+
+//contact
+const contactForm = document.querySelector("#contactForm");
+
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstName-error");
+
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#email-error");
+
+const message = document.querySelector("#message");
+const messageError = document.querySelector("#message-error");
+
+const success = document.querySelector(".success");
+const submitIfValid = document.querySelector(".submit");
+
+function contactValidation(e) {
+  e.preventDefault();
+  const firstNameLength = numberOfCharacters(firstName.value, 1);
+  const messageLength = numberOfCharacters(message.value, 1);
+  const emailCheck = emailValidation(email.value);
+
+  if (firstNameLength && messageLength && emailCheck) {
+    success.innerHTML =
+      '<div class="success">Your message has been successfully sent. I will contact you very soon!</div>';
+  } else {
+    success.innerHTML = "";
+  }
+  if (firstNameLength === true) {
+    firstNameError.style.display = "none";
+  } else {
+    firstNameError.style.display = "block";
+  }
+
+  if (emailCheck === true) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+  if (messageLength === true) {
+    messageError.style.display = "none";
+  } else {
+    messageError.style.display = "block";
+  }
+
+  contactForm.reset();
+}
+
+contactForm.addEventListener("submit", contactValidation);
+
+function numberOfCharacters(value, len) {
+  if (value.trim().length > len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function emailValidation(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
+
+const checkCharacterType = function checkIfOnlyLetter(field) {
+  if (/^[a-zA-Z ]+$/.test(field.value)) {
+    setValid(field);
+    return true;
+  } else {
+    setInvalid(field, `$(field.name)must contain only letter`);
+    return false;
+  }
+};
